ea5a2cb83da2dd35d2dc5b309f39aab00f1d8e56
Sotos <tsompilis@gmail.com>
2025-11-01 22:16:12 +0200

    added graphic

--- src/main/java/org/example/databasetechnologyproject/HelloController.java
+++ src/main/java/org/example/databasetechnologyproject/HelloController.java
@@ -6,6 +6,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import java.io.IOException;
 import java.net.URL;
@@ -17,9 +19,18 @@ import java.util.ResourceBundle;
 public class HelloController implements Initializable {
     @FXML
     Button exitButton;
+    @FXML
+    Tooltip tooltipexit;
     static Connection dbConnection = null;
     static String driverClassName = "org.postgresql.Driver";
     public void initialize(URL location, ResourceBundle arg1){
+        try{
+            Image image = new Image(getClass().getResourceAsStream("exit.png"));
+            ImageView imageview = new ImageView(image);
+            tooltipexit.setGraphic(imageview);
+        } catch (Exception ex){
+            System.out.println("Image not found");
+        }
         try {
             Class.forName(driverClassName);
         } catch (ClassNotFoundException ex){
