fd778f596d320c2ef00e0dae1e153566e12c1802
Sotos <tsompilis@gmail.com>
2025-11-02 02:30:43 +0200

    Added Alert Dialog and exit option

--- src/main/java/org/example/databasetechnologyproject/HelloController.java
+++ src/main/java/org/example/databasetechnologyproject/HelloController.java
@@ -1,26 +1,37 @@
 package org.example.databasetechnologyproject;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Tooltip;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    BorderPane scene1;
     @FXML
     Button exitButton;
     @FXML
     Tooltip tooltipexit;
+    private DialogPane dialog;
     static Connection dbConnection = null;
     static String driverClassName = "org.postgresql.Driver";
     public void initialize(URL location, ResourceBundle arg1){
@@ -40,6 +51,24 @@ public class HelloController implements Initializable {
             dbConnection = DatabaseConnection.getConnection();
         } catch (SQLException ex){
             System.out.println("Connection to database failed");
+        }
+    }
+    public void exitButton(ActionEvent event){
+        Alert alert2 = new Alert(Alert.AlertType.CONFIRMATION);
+        dialog = alert2.getDialogPane();
+        alert2.setGraphic(new ImageView(this.getClass().getResource("kk.png").toString()));
+        dialog.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        Image icon = new Image("logos.png");
+        dialog.getStyleClass().add("dialog2");
+        alert2.setTitle("Exit Application");
+        alert2.setHeaderText("You are About to Exit The Application");
+        alert2.setContentText("Are you sure you wish to exit the Application?");
+        Stage alertstage = (Stage) dialog.getScene().getWindow();
+        alertstage.getIcons().add(icon);
+        if (alert2.showAndWait().get() == ButtonType.OK) {
+            stage = (Stage) scene1.getScene().getWindow();
+            System.out.println("You exited the app");
+            stage.close();
         }
     }
 }
