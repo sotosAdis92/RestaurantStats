49cd3a3e39c7187527064e35c31821024a414e39
Sotos <tsompilis@gmail.com>
2025-11-05 00:34:50 +0200

    added ui to textfields

--- src/main/java/org/example/databasetechnologyproject/CustomerInsertController.java
+++ src/main/java/org/example/databasetechnologyproject/CustomerInsertController.java
@@ -3,8 +3,10 @@ package org.example.databasetechnologyproject;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -23,9 +25,53 @@ public class CustomerInsertController implements Initializable {
     TableColumn<Customer, String> numberColumn;
     @FXML
     TableColumn<Customer, String> emailColumn;
+    @FXML
+    TextField textField1;
+
+    @FXML
+    TextField textField2;
+
+    @FXML
+    TextField textField3;
+
+    @FXML
+    TextField textField4;
+
+    @FXML
+    TextField textField5;
+
+    @FXML
+    Label emailLabel;
+
+    @FXML
+    Label firstNameLabel;
+
+    @FXML
+    Label homeLabel;
+
+    @FXML
+    Label lastNameLabel;
+
+    @FXML
+    Label phoneLabel;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        if(textField1.isFocused()){
+            firstNameLabel.setStyle("-fx-background-color: #FF5C00");
+        }
+        else if(textField2.isFocused()){
+            lastNameLabel.setStyle("-fx-background-color: #FF5C00");
+        }
+        else if(textField3.isFocused()){
+            homeLabel.setStyle("-fx-background-color: #FF5C00");
+        }
+        else if(textField4.isFocused()){
+            phoneLabel.setStyle("-fx-background-color: #FF5C00");
+        }
+        else if(textField5.isFocused()){
+            emailLabel.setStyle("-fx-background-color: #FF5C00");
+        }
     }
     public void setMainController(CustomerController mainController){
         this.mainController = mainController;
