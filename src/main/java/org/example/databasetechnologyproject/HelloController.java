5fdc6f6f185f3d15817876edf13681208e32e901
Sotos <tsompilis@gmail.com>
2025-11-25 00:38:15 +0200

    started home page, graphs added

--- src/main/java/org/example/databasetechnologyproject/HelloController.java
+++ src/main/java/org/example/databasetechnologyproject/HelloController.java
@@ -110,6 +110,7 @@ public class HelloController implements Initializable {
                                   height: 390px;
                                   background: white;
                                   border-radius: 10px;
+                                  margin-top: 50px;
                                   padding: 0px;
                                   box-shadow: 0 4px 12px rgba(0,0,0,0.1);
                               }
